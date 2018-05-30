9b6da2837755e67b1e9d96e99b11d29e54962858
williamd4112 <williamd4112@hotmail.com>
2018-05-30 20:04:05 +0800

    add lambd schedule

--- simulator/city.py
+++ simulator/city.py
@@ -5,21 +5,27 @@ from simulator.city_graph import CityGraph
 from simulator.city_customer_call_simulation import CityCustomerCallSimulation
  
 class City(object):
-    def __init__(self, intersections, initial_hour, lambd_schedule):
+    def __init__(self, intersections, initial_hour, lambd_schedule=[]):
         '''
         intersections: specification of intersections
         initial_hour: initial time
         lambd_schedule: schedule of lambda in Poisson process
         '''
         self.intersections = intersections
+        self.lambd_schedule = lambd_schedule
         self.city_graph = CityGraph(self.intersections)
         self.time_sys = TimeSystem(initial_hour)
-
+        
         self.customer_call_sim = CityCustomerCallSimulation(self.intersections, self.city_graph, self.time_sys)
               
     def step(self):
-        # TODO: Setup the hyperparamters according to current time
-        self.customer_call_sim.set(lambd=3)     
+        # Default lambd is 1.0
+        self.customer_call_sim.set(lambd=1.0)
+        current_hour_in_day = self.time_sys.hour_in_a_day()
+        for start_hour_in_day, end_hour_in_day, lambd in self.lambd_schedule:
+            if current_hour_in_day >= start_hour_in_day and current_hour_in_day < end_hour_in_day:
+                self.customer_call_sim.set(lambd=lambd)
+                break     
 
         # Generate customers' calls with the city_graph.
         customer_calls = self.customer_call_sim()
--- test_taxi_coordinator.py
+++ test_taxi_coordinator.py
@@ -67,14 +67,14 @@ def test_payoff_bidding():
         (5,0), (5,2), (5,4), (5,6), (5,8) , (5,12), (5,14),
         (7,0), (7,2), (7,4), (7,6), (7,8) , (7,12), (7,14),
     ]
-    city = City(intersections, 0, None)
+    city = City(intersections, 0)
     coordinator = TaxiCoordinator(city=city, 
                 auction_type='second-price', 
                 drivers_schedule=[  [],
                                     [],
                                     []],
                 init_pos=(4, 8))
-    while city.time_sys.hour_in_sim() < 24*7:
+    while city.time_sys.hour_in_sim() < 24:
         customer_calls = city.step() 
         coordinator.allocate(customer_calls)
     for driver in coordinator.drivers:
@@ -139,6 +139,33 @@ def test_shifts_with_weekly_schedule():
         for e in schedule.events:
             print(e)
 
+def test_payoff_bidding_city_lambd_schedule():
+    from util.common import daily_schedules_to_weekly_schedules
+    intersections = [ 
+	    # (x,y) is the place of a intersection
+        (0,0), (0,2), (0,6), (0,8), (0,12), (0,14),
+        (2,0), (2,2), (2,6), (2,7), (2,8) , (2,12), (2,14),
+        (3,0), (3,2), (3,6), (3,7), (3,8) , (3,12), (3,14),
+        (4,0), (4,2), (4,4), (4,6), (4,7) , (4,8) , (4,12), (4,14),
+        (5,0), (5,2), (5,4), (5,6), (5,8) , (5,12), (5,14),
+        (7,0), (7,2), (7,4), (7,6), (7,8) , (7,12), (7,14),
+    ]
+    lambd_schedule = [(7, 9, 3.0), (17, 19, 3.0), (9, 17, 2.0), (19, 23, 2.0)]
+    city = City(intersections, 0, lambd_schedule)
+    daily_schedule_for_drivers = [  [(8, 12), (14, 23)],
+                                    [(10, 12)]]
+    weekly_schedule_for_drivers = daily_schedules_to_weekly_schedules(daily_schedule_for_drivers)
+    coordinator = TaxiCoordinator(city=city, 
+                auction_type='second-price', 
+                drivers_schedule=weekly_schedule_for_drivers,
+                init_pos=(4, 8))
+    while city.time_sys.hour_in_sim() < 24:
+        customer_calls = city.step() 
+        coordinator.allocate(customer_calls)
+    for driver in coordinator.drivers:
+        print('Driver-{} payoff {}'.format(driver.idx, driver.get_payoff()))
+    print('Company payoff', coordinator.get_payoff())
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -153,3 +180,5 @@ if __name__ == '__main__':
         test_dump_json()
     elif args.case == 'shift_weekly':
         test_shifts_with_weekly_schedule()
+    elif args.case == 'payoff_bidding_with_city_lambd_schedule':
+        test_payoff_bidding_city_lambd_schedule()
