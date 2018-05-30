e8830b3be9ac298e7f5fcbd51e0f892fd4ddd9af
williamd4112 <williamd4112@hotmail.com>
2018-05-30 19:49:08 +0800

    add daily schedule to weekly

--- test_taxi_coordinator.py
+++ test_taxi_coordinator.py
@@ -109,6 +109,37 @@ def test_dump_json():
         print(e)
     schedule.dump_json(os.path.join('data', 'driver_0.json'))
 
+def test_shifts_with_weekly_schedule():
+    from util.common import daily_schedules_to_weekly_schedules
+    intersections = [ 
+	    # (x,y) is the place of a intersection
+        (0,0), (0,2), (0,6), (0,8), (0,12), (0,14),
+        (2,0), (2,2), (2,6), (2,7), (2,8) , (2,12), (2,14),
+        (3,0), (3,2), (3,6), (3,7), (3,8) , (3,12), (3,14),
+        (4,0), (4,2), (4,4), (4,6), (4,7) , (4,8) , (4,12), (4,14),
+        (5,0), (5,2), (5,4), (5,6), (5,8) , (5,12), (5,14),
+        (7,0), (7,2), (7,4), (7,6), (7,8) , (7,12), (7,14),
+    ]
+    city = City(intersections, 0, None)
+    daily_schedule_for_drivers = [  [(8, 12), (14, 23)],
+                                    [(10, 12)]]
+    weekly_schedule_for_drivers = daily_schedules_to_weekly_schedules(daily_schedule_for_drivers)
+    coordinator = TaxiCoordinator(city=city, 
+                auction_type='second-price', 
+                drivers_schedule=weekly_schedule_for_drivers,
+                init_pos=(4, 8))
+    print(coordinator.drivers)
+    customer_calls = [  CustomerCall((4, 4), (5, 4), 4),
+                        CustomerCall((5, 6), (5, 8), 9),
+                        CustomerCall((4, 7), (4, 12), 15)]
+    coordinator.allocate(customer_calls)
+    for driver in coordinator.drivers:
+        schedule = driver.generate_complete_schedule(24, True)
+        print('Schedule for driver-{}'.format(driver.idx))
+        for e in schedule.events:
+            print(e)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--case', type=str, required=True)
@@ -120,3 +151,5 @@ if __name__ == '__main__':
         test_start_from_shift()
     elif args.case == 'dump_json':
         test_dump_json()
+    elif args.case == 'shift_weekly':
+        test_shifts_with_weekly_schedule()
--- util/common.py
+++ util/common.py
@@ -6,4 +6,18 @@ def check_duplicated_element(l):
 def movement_to_dir(mov):
     direction = (np.sign(mov[0]), np.sign(mov[1]))
     return direction
- 
+
+def daily_schedules_to_weekly_schedules(daily_schedule_for_drivers):
+    '''
+    Pass the daily schedules, convert to weekly schedule for each drivers.
+    '''
+    weekly_schedules_for_drivers = []
+    for daily_schedule_for_driver in daily_schedule_for_drivers:
+        weekly_schedules_for_driver = []
+        for start_time, end_time in daily_schedule_for_driver:
+            for day in range(7):
+                weekly_schedules_for_driver.append((24 * day + start_time, 24 * day + end_time))
+        weekly_schedules_for_drivers.append(weekly_schedules_for_driver)
+    return weekly_schedules_for_drivers
+        
+        
